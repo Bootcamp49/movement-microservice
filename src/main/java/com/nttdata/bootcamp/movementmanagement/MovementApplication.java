1c560418cfbb06dd2c359f685b2a8c859c313f94
Flavio Castillo <flavio.castillo@hotmail.com>
2024-02-08 23:45:02 -0500

    Added library and configuration to enable discoveryClient

--- src/main/java/com/nttdata/bootcamp/movementmanagement/MovementApplication.java
+++ src/main/java/com/nttdata/bootcamp/movementmanagement/MovementApplication.java
@@ -2,10 +2,12 @@ package com.nttdata.bootcamp.movementmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * Clase principal para ejecutar el microservicio de movimientos.
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 public class MovementApplication {
     public static void main(String[] args) {
